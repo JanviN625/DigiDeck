bb7d67bfe6cb45d435fd2d2193c6fcbbf3967f3e
Evelyn Hosana <evelynhosana123@gmail.com>
2026-02-12 18:41:45 -0500

    Removed failing.test.js for deployment testing
    testing vercel deployment, must remove failed test

--- src/tests/failing.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import App from '../App';
-
-test('the app displays a Get Started button - should not exist = failure', () => {
-  render(<App />);
-  const buttonElement = screen.getByText(/get started/i);
-  expect(buttonElement).toBeInTheDocument();
-});
