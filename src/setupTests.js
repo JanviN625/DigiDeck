2d8eb719e03adf176c2f86f032a8c48cea5caa8c
JanviN625 <janvinandwani@gmail.com>
2026-04-23 00:30:02 -0400

    feat: initialize test environment with jest-dom and firebase mocks

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,23 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
+jest.mock('./firebase/firebaseConfig', () => {
+    return {
+        auth: {},
+        db: {},
+        storage: {},
+    };
+});
+
+jest.mock('firebase/auth', () => ({
+    onAuthStateChanged: (auth, cb) => {
+        cb(null);
+        return () => {};
+    },
+}));
+
 beforeAll(() => {
     jest.spyOn(console, 'error').mockImplementation(() => {});
+    jest.spyOn(console, 'log').mockImplementation(() => {});
+    jest.spyOn(console, 'warn').mockImplementation(() => {});
 });
